7f6db96ab9da70d1aeafc7861b9d71c4f0f9a128
Dmitriy <ulrht88@gmail.com>
2025-08-24 04:21:06 +0300

    fix: fixed relations on models.py

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -16,12 +17,14 @@ class User(Base):
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
     
     search_filter: Mapped['SearchFilter'] = relationship(back_populates='user', cascade="all, delete-orphan")
+    blacklist: Mapped['BlackList'] = relationship(back_populates='user', cascade="all, delete-orphan")
+    token: Mapped['Token'] = relationship(back_populates='user', cascade="all, delete-orphan")
 
 
 class SearchFilter(Base):
     __tablename__ = 'search_filters'
     
-    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
     resume_id: Mapped[str] = mapped_column(nullable=False)
     key_words: Mapped[str] = mapped_column(nullable=False)
     experience: Mapped[str] = mapped_column(nullable=True)
@@ -32,29 +35,28 @@ class SearchFilter(Base):
     search_field: Mapped[str] = mapped_column(nullable=True)
     message: Mapped[str] = mapped_column(nullable=True)
     
-    user: Mapped['User'] = relationship(back_populates='search_filter', cascade="all, delete-orphan")
-    blacklist: Mapped['BlackList'] = relationship(back_populates='user', cascade="all, delete-orphan")
-    token: Mapped['Token'] = relationship(back_populates='user', cascade="all, delete-orphan")
+    user: Mapped['User'] = relationship(back_populates='search_filter')
+
 
 
 class BlackList(Base):
     __tablename__ = 'black_list'
     
-    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
     companies: Mapped[list[str]] = mapped_column(JSONB, nullable=True)
     
-    user: Mapped['User'] = relationship(back_populates='blacklist', cascade="all, delete-orphan")
+    user: Mapped['User'] = relationship(back_populates='blacklist')
 
 
 class Token(Base):
     __tablename__ = 'tokens'
     
-    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
     access_token: Mapped[str] = mapped_column(nullable=True)
     refresh_token: Mapped[str] = mapped_column(nullable=True)
     create_or_update: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now)
     
-    user: Mapped['User'] = relationship(back_populates='token', cascade="all, delete-orphan")
+    user: Mapped['User'] = relationship(back_populates='token')
 
 
 class UserBlackList(Base):
